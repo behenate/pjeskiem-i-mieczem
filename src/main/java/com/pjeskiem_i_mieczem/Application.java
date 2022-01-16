822c92fe4b5d7b6137d8611a0a1d2e38541abfa6
zfurtak <zfurtak@student.agh.edu.pl>
2022-01-16 18:17:06 +0100

    hello tu zuzia

--- src/main/java/com/pjeskiem_i_mieczem/Application.java
+++ src/main/java/com/pjeskiem_i_mieczem/Application.java
@@ -12,7 +12,9 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         ImageButton testButton = new ImageButton("Testowy teścik", 300, 100, "buttons/button_0.png");
         WelcomeScreenGui welcomeScreenGui = new WelcomeScreenGui();
-        Scene scene = new Scene(welcomeScreenGui, Config.windowWidth, Config.windowHeight);
+        //Scene scene = new Scene(welcomeScreenGui, Config.windowWidth, Config.windowHeight);
+        LeaderboardGui leaderboardGui = new LeaderboardGui();
+        Scene scene = new Scene(leaderboardGui, Config.windowWidth, Config.windowHeight);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
