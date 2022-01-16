8ea06017c9c4a06aa059a7e6d0c95e5d927d9053
behenate <behenate@gmail.com>
2022-01-17 00:13:29 +0100

    Gui walki i klasy dla każdej z klas postaci

--- src/main/java/com/pjeskiem_i_mieczem/Application.java
+++ src/main/java/com/pjeskiem_i_mieczem/Application.java
@@ -10,14 +10,19 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Player hunterPreset = new Player("Łowca", 2, 2, 2, 100, 2, "dino.jpg");
+        hunterPreset.setName("Dino");
+        Player warriorPreset = new Player("Wojownik", 2, 2, 2, 2, 2, "zloty.jpg");
+        warriorPreset.setName("Złoty");
         ImageButton testButton = new ImageButton("Testowy teścik", 300, 100, "buttons/button_0.png");
         WelcomeScreenGui welcomeScreenGui = new WelcomeScreenGui();
         CreateCharacterGui createCharacterGui = new CreateCharacterGui();
         LeaderboardGui leaderboardGui = new LeaderboardGui();
+        FightGui fightGui = new FightGui(hunterPreset, warriorPreset);
         //Scene scene = new Scene(welcomeScreenGui, Config.windowWidth, Config.windowHeight);
-        Scene scene = new Scene(leaderboardGui, Config.windowWidth, Config.windowHeight);
+        Scene scene = new Scene(fightGui, Config.windowWidth, Config.windowHeight);
         //Scene scene = new Scene(createCharacterGui, Config.windowWidth, Config.windowHeight);
-        stage.setTitle("Hello!");
+        stage.setTitle("Pjeskiem I Mieczem");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/pjeskiem_i_mieczem/Player.java
+++ src/main/java/com/pjeskiem_i_mieczem/Player.java
@@ -1,9 +1,12 @@
 package com.pjeskiem_i_mieczem;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class Player {
     protected String name;
@@ -15,23 +18,23 @@ public class Player {
     protected Statistic intelligence;
     protected Statistic luck;
     protected String imagePath;
-    protected Image classImage;
-    protected ImageView classImageView;
+    protected Image image;
+    protected ImageView imageView;
     public Player(String className, Integer endurance, Integer strength, Integer dexterity,
                   Integer intelligence, Integer luck, String imagePath){
         this.className = className;
         this.endurance = new Statistic("Wytrzymałość",endurance);
         this.strength = new Statistic("Siła",strength);
         this.dexterity = new Statistic("Zręczność", dexterity);
-        this.intelligence = new Statistic("Intencja", intelligence);
+        this.intelligence = new Statistic("Inteligencja", intelligence);
         this.luck = new Statistic("Szczęście", luck);
         this.imagePath = imagePath;
-        this.hp = new Statistic("Hp", this.endurance.getValue()*10 );
+        this.hp = new Statistic("Temp HP", this.endurance.getValue()*1);
         setImagePath(imagePath);
     }
     public void setImagePath(String imagePath){
-        this.classImage = new Image(imagePath);
-        this.classImageView = new ImageView(this.classImage);
+        this.image = new Image(imagePath);
+        this.imageView = new ImageView(this.image);
     }
     public void setName(String name){
         this.name = name;
@@ -40,9 +43,21 @@ public class Player {
         return new VBox(
                 hp.getLabel(),
                 strength.getLabel(),
+                intelligence.getLabel(),
                 dexterity.getLabel(),
                 endurance.getLabel(),
                 luck.getLabel()
         );
+    }
+    public VBox getPlayerCard(int width){
+        Label playerNameLabel = new Label(name);
+        playerNameLabel.setFont(Font.font(29));
+        StatBar healthBar = new StatBar("#f7573e", true,width-20, (int)(Config.windowHeight*0.05), hp.getValue(), 5);
+        Node statCard = getStatsView();
+        imageView.setFitWidth(width*0.7);
+        imageView.setFitHeight(width*0.7);
+        VBox playerCard = new VBox(playerNameLabel, imageView, healthBar, statCard);
+        playerCard.setAlignment(Pos.CENTER);
+        return playerCard;
     }
 }
